590e5b6b4f7aa60c8c932711b5a593ca81cd36b5
Bhuval-Singh <ayushsinghbca2022@gmail.com>
2024-10-18 10:36:34 +0530

    18 Oct

---
+++ GreedyApproach/IndianCoinChange.java
@@ -0,0 +1,22 @@
+/* This program calculates the minimum number of Indian coins/notes required to make a given amount. */
+import java.util.*;
+
+public class IndianCoinChange {
+    public static void main(String[] args) {
+        Integer coin[] = { 1, 2, 5, 10, 20, 50, 100, 500, 2000 };
+        int amount = 122;
+        int count = 0;
+        Arrays.sort(coin, Comparator.reverseOrder());
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0;i<coin.length;i++){
+            if(coin[i] <= amount){
+                while(coin[i] <= amount){
+                    count++;
+                    ans.add(coin[i]);
+                    amount -= coin[i];
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
